b2abbef9de9a2826d4b5503bd19d4b7e671a1685
Wolf-dev-21 <wolf.of.art21@gmail.com>
2024-04-15 19:38:32 -0300

    atv Final pt1 completa

--- atv4/src/pages/Home/Home.js
+++ atv4/src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import styles from './Home.module.css'
+import './Home.module.css'
 import Input from '../../components/Nome/Nome'
 import { Select } from '../../components/select/Select';
 import { useEffect } from 'react'
@@ -27,5 +27,34 @@ export default function Home() {
         }
       )
 }, [])
+
+return(
+
+  <div className='home'>
+    <h1> Turmas</h1>
+
+    <form>
+
+    <Input
+      type="text"
+      name="nome da turma"
+      id="id_nome"
+      placeholder="Turma:"
+      text="Digitar o nome da turma"
+    />
+
+    <Select
+    name="sigla da turma"
+    id="id_sigla"
+    options={sigla}
+    text="Selecione a sigla referente a Turma"
+    />
+
+
+    </form>
+
+  </div>
+
+)
 
 }
