20ca32869773799ac1bcd7dfcd34b3817757e245
Wolf-dev-21 <wolf.of.art21@gmail.com>
2024-04-25 22:22:15 -0300

    atv5

--- atv4/src/pages/Home/Home.js
+++ atv4/src/pages/Home/Home.js
@@ -1,14 +1,23 @@
-import './Home.module.css'
-import Input from '../../components/Nome/Nome'
+import { useEffect, useState } from 'react'
+
+import {useNavigate} from 'react-router-dom';
+
+import Input from '../../components/Nome/Nome';
 import { Select } from '../../components/select/Select';
-import { useEffect } from 'react'
+
+import style from './Home.module.css'
+
+
+
 
 export default function Home() {
   const [sigla, setSigla] = useState([]);
 
+  const [nome, setNome] = useState([]);
+
   useEffect(() => {
     fetch(
-      'http://localhost:5000/turmas',
+      'http://localhost:5000/sigla',
       {
         method:'get',
         headers:{
@@ -28,12 +37,51 @@ export default function Home() {
       )
 }, [])
 
+function handlerOnChanceNome(event) {
+  setTurma({...nome, [event.target.name] : event.target.value});
+  console.log(nome)
+}
+
+function handlerOnChanceSigla(event){
+  setSigla({...nome, sigla:{
+    id: event.target.value,
+    sigla: event.target.options[event.target.selelectedIndex].text
+  }});
+}
+
+function createNome(nome){
+  fetch('http://localhost:5000/turma',{
+    method:'post',
+    headers:{
+      'content-type':'application/json'
+    },
+    body: JSON.stringify(nome)
+  })
+    .then(
+      (resp) => resp.json()
+    )
+    .then(
+      (data) => {
+        console.log(data)
+        navigate('./Nome',{state:'Turma cadastrada'})
+      }
+    )
+    .catch(
+      (error)=>{console.log(error)}
+    )
+
+    function submit(event){
+      event.preventDefault();
+      createNome(nome);
+    }
+}
+
 return(
 
   <div className='home'>
     <h1> Turmas</h1>
 
-    <form>
+    <form onSubmit={submit}>
 
     <Input
       type="text"
@@ -41,6 +89,7 @@ return(
       id="id_nome"
       placeholder="Turma:"
       text="Digitar o nome da turma"
+      handlerOnChange={handlerOnChanceNome}
     />
 
     <Select
@@ -48,8 +97,12 @@ return(
     id="id_sigla"
     options={sigla}
     text="Selecione a sigla referente a Turma"
+    handlerOnChange={handlerOnChanceSigla}
     />
 
+        <p>
+          <button type="submit">Enviar</button>
+        </p>
 
     </form>
 
